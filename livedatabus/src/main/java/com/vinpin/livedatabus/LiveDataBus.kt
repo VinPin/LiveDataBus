75402af72e585368bb8999cafa21dba826fc1aa2
VinPin <hearzwp@163.com>
2021-10-15 09:03:57 +0800

    方法添加注释

--- livedatabus/src/main/java/com/vinpin/livedatabus/LiveDataBus.kt
+++ livedatabus/src/main/java/com/vinpin/livedatabus/LiveDataBus.kt
@@ -10,6 +10,9 @@ object LiveDataBus {
 
     private val mBus: MutableMap<String, EventLiveData<*>> = mutableMapOf()
 
+    /**
+     * 根据[key]获取指定的[EventLiveData]，如果不存在则会自动创建。
+     */
     fun <T> get(key: String): EventLiveData<T>? {
         synchronized(mBus) {
             if (!mBus.containsKey(key)) {
@@ -19,18 +22,27 @@ object LiveDataBus {
         }
     }
 
+    /**
+     * 获取当前事件的总数量
+     */
     fun count(): Int {
         synchronized(mBus) {
             return mBus.size
         }
     }
 
+    /**
+     * 清除所有的事件
+     */
     fun clear() {
         synchronized(mBus) {
             mBus.clear()
         }
     }
 
+    /**
+     * 清除[key]的事件
+     */
     fun clear(key: String) {
         synchronized(mBus) {
             mBus.remove(key)
