e1ae484fe0e40b455b398dff716348a9ceaeabfb
VinPin <zhangwenping0114@zto.com>
2025-12-31 11:17:11 +0800

    feat: 使用ConcurrentHashMap替换synchronized

--- livedatabus/src/main/java/com/vinpin/livedatabus/LiveDataBus.kt
+++ livedatabus/src/main/java/com/vinpin/livedatabus/LiveDataBus.kt
@@ -1,51 +1,43 @@
 package com.vinpin.livedatabus
 
+import java.util.concurrent.ConcurrentHashMap
+
 /**
  * author : VinPin
  * e-mail : hearzwp@163.com
- * time   : 2021/10/14 20:40
+ * time   : 2020/8/21 21:29
  * desc   : 基于LiveData实现的事件总线EventBus。
  */
 object LiveDataBus {
 
-    private val mBus: MutableMap<String, EventLiveData<*>> = mutableMapOf()
+    private val mBus = ConcurrentHashMap<String, EventLiveData<*>>()
 
     /**
      * 根据[key]获取指定的[EventLiveData]，如果不存在则会自动创建。
      */
     fun <T> get(key: String): EventLiveData<T>? {
-        synchronized(mBus) {
-            if (!mBus.containsKey(key)) {
-                mBus[key] = EventLiveData<T>()
-            }
-            return mBus[key] as? EventLiveData<T>
-        }
+        mBus.putIfAbsent(key, EventLiveData<T>())
+        return mBus[key] as? EventLiveData<T>
     }
 
     /**
      * 获取当前事件的总数量
      */
     fun count(): Int {
-        synchronized(mBus) {
-            return mBus.size
-        }
+        return mBus.size
     }
 
     /**
      * 清除所有的事件
      */
     fun clear() {
-        synchronized(mBus) {
-            mBus.clear()
-        }
+        mBus.clear()
     }
 
     /**
      * 清除[key]的事件
      */
     fun clear(key: String) {
-        synchronized(mBus) {
-            mBus.remove(key)
-        }
+        mBus.remove(key)
     }
 }
